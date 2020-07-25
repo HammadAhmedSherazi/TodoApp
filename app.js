9b2b8b824306fa86d3688f7d32d954f7bd211ea9
HammadAhmedSherazi <46845004+HammadAhmedSherazi@users.noreply.github.com>
2020-07-25 20:46:13 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ function addItem(){
 
     //Input shift into list through create Li in the list
     var li = document.createElement('li');
-    li.innerHTML = item.value;
+    //li.innerHTML = item.value;
+    
+    var text = document.createElement('span');
+    text.append(item.value);
+    li.appendChild(text);
 
     list.appendChild(li);
     //create checkbox
@@ -64,18 +68,19 @@ function check(thisNode){
 	var parent = thisNode.parentNode;
 	parent.style.opacity = '0.5';
 	del = document.createElement('del');
-	var txt = parent.childNodes[0].nextSibling.nodeValue;
-	var editedItemTxt = document.createTextNode(txt);
-	del.appendChild(editedItemTxt);
+	var txt = parent.childNodes[0].nextSibling.innerHTML;
+	var editedtxt = document.createTextNode(txt);
+	del.appendChild(editedtxt);
 	parent.appendChild(del);
 	parent.insertBefore(parent.lastChild, parent.childNodes[0].nextSibling);
 	parent.childNodes[2].remove();
 }
 function editItem(thisNode){
+	var parent = thisNode.parentNode.parentNode;
     var inp = document.createElement('input');
     inp.setAttribute('id','edtinp');
     
-    var checkBox = document.getElementById('check');
+    var checkBox = parent.firstChild;
     checkBox.style.display='none';
    
     //create button to add Edited Item
@@ -85,43 +90,41 @@ function editItem(thisNode){
     addbtn.setAttribute('id', 'addEdtbtn');
     addbtn.innerHTML = "Add";
 
-    var li = thisNode.parentNode.parentNode;
-    li.appendChild(inp);
-    li.insertBefore(li.lastChild, li.firstChild);
+    parent.appendChild(inp);
+    parent.insertBefore(parent.lastChild, parent.firstChild);
 
-    var txt = li.childNodes[2].nodeValue;
-    if(txt == null){
-    inp.value = "";
-    inp.setAttribute('placeholder', 'Please Re-enter todo item');
-    thisNode.parentNode.parentNode.childNodes[2].remove();
-    }
-    else{
+    var txt = parent.childNodes[2].innerHTML;
+    
     inp.setAttribute('value', txt );
-    thisNode.parentNode.parentNode.childNodes[2].remove();}
-   
-
+    //console.log(li.childNodes[2])
+    parent.childNodes[2].remove();
+    
     thisNode.parentNode.appendChild(addbtn);
     var span1 = thisNode.parentNode;
     span1.insertBefore(span1.lastChild, span1.firstChild);
     span1.childNodes[1].remove();
+    
 
 }
 function addEdit(thisNode) {
 
+	var parent = thisNode.parentNode.parentNode;
 
     var editbtn = document.createElement('button');
     editbtn.setAttribute('class', 'btn btn-success');
     editbtn.setAttribute('onclick', 'editItem(this)');
     editbtn.innerHTML = "Edit";
     
-    var checkBox = document.getElementById('check');
+   
+    
+    var checkBox = parent.childNodes[1];
     checkBox.style.display='inline';
 
     var span1 = thisNode.parentNode;
     span1.appendChild(editbtn);
 
     var edinp = document.getElementById('edtinp')
-    let txt = edinp.value;
+    var txt = edinp.value;
 
     var editedItem = document.createElement('span');
     var editedItemTxt = document.createTextNode(txt);
